9023dbeea5f18494627b0b68f95105db7e6ee9dc
José Moreira <52895465+joseafmoreira@users.noreply.github.com>
2024-11-05 15:22:03 +0000

    Added some code

--- crisiswatcher/src/main/java/dev/joseafmoreira/db/DB.java
+++ crisiswatcher/src/main/java/dev/joseafmoreira/db/DB.java
@@ -1,7 +1,9 @@
 package dev.joseafmoreira.db;
 
+import dev.joseafmoreira.db.server.Server;
+
 public class DB {
     public static void main(String[] args) {
-        
+        new Server();
     }
 }
--- crisiswatcher/src/main/java/dev/joseafmoreira/db/server/Server.java
+++ crisiswatcher/src/main/java/dev/joseafmoreira/db/server/Server.java
@@ -5,16 +5,18 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 
 public class Server {
-    private static final int port = 1433;
-    private static final int backlog = 50;
-    private static final InetAddress address = InetAddress.getLoopbackAddress();
+    private static final int PORT = 1433;
+    private static final int BACKLOG = 50;
+    private static final InetAddress ADDRESS = InetAddress.getLoopbackAddress();
+    private static final int SO_TIMEOUT = 2500;
     private ServerSocket serverSocket;
 
     public Server() {
         try {
-            serverSocket = new ServerSocket(port, backlog, address);
+            serverSocket = new ServerSocket(PORT, BACKLOG, ADDRESS);
+            serverSocket.setSoTimeout(SO_TIMEOUT);
         } catch (IOException e) {
-            e.printStackTrace();
+            
         }
     }
 }
