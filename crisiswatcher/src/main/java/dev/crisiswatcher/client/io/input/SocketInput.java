61df48c56ae1d19e49d26be770696b65b9862ca2
José Moreira <52895465+joseafmoreira@users.noreply.github.com>
2024-11-27 21:16:19 +0000

    Update SocketInput.java

--- crisiswatcher/src/main/java/dev/crisiswatcher/client/io/input/SocketInput.java
+++ crisiswatcher/src/main/java/dev/crisiswatcher/client/io/input/SocketInput.java
@@ -3,7 +3,10 @@ package dev.crisiswatcher.client.io.input;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+import dev.crisiswatcher.client.dto.RoomDTO;
 import dev.crisiswatcher.client.dto.UserDTO;
 import dev.crisiswatcher.client.dto.UserDTO.UserProfile;
 import dev.crisiswatcher.client.io.IOSharedResources;
@@ -41,6 +44,10 @@ public class SocketInput extends Thread {
      */
     private static final String DEFAULT_CLOSE_MESSAGE = "Até à próxima";
     /**
+     * Message displayed when the room is changed
+     */
+    private static final String DEFAULT_ROOM_CHANGE_MESSAGE = "Sala alterada";
+    /**
      * The socket input buffered reader
      */
     private BufferedReader socketInput;
@@ -48,6 +55,10 @@ public class SocketInput extends Thread {
      * The user data transfer object
      */
     private UserDTO userDTO;
+    /**
+     * The room data transfer object
+     */
+    private RoomDTO roomDTO;
 
     /**
      * Constructs a new SocketInput object with a specified socketInputStreamReader and ioSharedResources.
@@ -58,6 +69,7 @@ public class SocketInput extends Thread {
     public SocketInput(InputStreamReader socketInputStreamReader, IOSharedResources ioSharedResources) {
         socketInput = new BufferedReader(socketInputStreamReader);
         userDTO = ioSharedResources.getUserDTO();
+        roomDTO = ioSharedResources.getRoomDTO();
     }
 
     /**
@@ -76,6 +88,14 @@ public class SocketInput extends Thread {
                     userDTO.setName((splittedOutput[1].equals("null")) ? null : splittedOutput[1]);
                     userDTO.setProfile((splittedOutput[2].equals("null")) ? null : UserProfile.getEnum(splittedOutput[2]));
                     output = splittedOutput[3].replaceAll("_", " ");
+                } else if (output.startsWith("/room")) {
+                    String[] splittedOutput = output.split(" ");
+                    roomDTO.setName((splittedOutput[1].equals("null")) ? null : splittedOutput[1]);
+                    try {
+                        roomDTO.setIp((splittedOutput[2].equals("null")) ? null : InetAddress.getByName(splittedOutput[2]));
+                    } catch (UnknownHostException ignored) {}
+                    roomDTO.setPort(Integer.valueOf(splittedOutput[3]));
+                    output = DEFAULT_ROOM_CHANGE_MESSAGE;
                 }
                 System.out.println(output.equals(DEFAULT_OUTPUT_MESSAGE) ? INVALID_COMMAND_MESSAGE : output);
             }
